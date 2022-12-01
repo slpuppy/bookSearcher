b0d3fce2871065f9cc37c441bb626e4f2a3149e8
gabriel <puppizin@gmail.com>
2022-11-30 22:25:30 -0300

    Initial Commit

---
+++ References/ReferencesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReferencesApp.swift
+//  References
+//
+//  Created by Gabriel Puppi on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ReferencesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
